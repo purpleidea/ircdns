bd4b17bf29de65b08e44ba8e56b562ddb285930e
James Shubin <james@shubin.ca>
2020-02-17 18:36:03 -0500

    misc: Improve make deps script

--- misc/make-deps.sh
+++ misc/make-deps.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-dnf install golang make go-bindata
+sudo_command=$(command -v sudo)
+
+YUM=`command -v yum 2>/dev/null`
+DNF=`command -v dnf 2>/dev/null`
+# if DNF is available use it
+if [ -x "$DNF" ]; then
+	YUM=$DNF
+fi
+
+$sudo_command $YUM install -y golang make go-bindata
 go get -u ./...
